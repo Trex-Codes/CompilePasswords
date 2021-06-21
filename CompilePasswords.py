a58d4ac74a2aa1ee1aa5c0947f99d65a2b9662f0
Trex-Codes <juan.13JSG@gmail.com>
2021-06-20 19:21:13 -0500

    11.0 FIX some code in the file with vars and info

--- CompilePasswords.py
+++ CompilePasswords.py
@@ -1,132 +1,138 @@
-import os
-import ctypes, sys
+
+import os 
+import ctypes, sys 
 from subprocess import call
 
-import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
+# Colors 
+COLOR_BANNER = '\033[0;36m'
+ERROR = '\033[1;31m'
+ERROR_DATA = '\033[4;31m'
+SUCCESS = '\033[1;32m'
+MESSAGE = '\033[0;33m'
+MESSAGE2 = '\033[0;35m'
+MESSAGE3 = '\033[1;35m'
+MESSAGE4 = '\033[1;34m'
+END = '\033[0;m'
 
-print("\033[0;36m", "  _____                      ___          _             ",  "\033[0;m")
-print("\033[0;36m", " /__   \_ __ _____  __      / __\___   __| | ___  ___   ",  "\033[0;m")
-print("\033[0;36m", "   / /\/ '__/ _ \ \/ /____ / /  / _ \ / _` |/ _ \/ __|  ",  "\033[0;m")
-print("\033[0;36m ", " / /  | | |  __/>  <_____/ /__| (_) | (_| |  __/\__ \   ",  "\033[0;m")
-print("\033[0;36m", "  \/   |_|  \___/_/\_\    \____/\___/ \__,_|\___||___/  ",  "\033[0;m")
+Banner = COLOR_BANNER + '''
+___________                               _________            .___             
+\__    ___/______   ____ ___  ___         \_   ___ \  ____   __| _/____   ______
+  |    |  \_  __ \_/ __ \\  \/  /  ______ /    \  \/ /  _ \ / __ |/ __ \ /  ___/
+  |    |   |  | \/\  ___/ >    <  /_____/ \     \___(  <_> ) /_/ \  ___/ \___ \ 
+  |____|   |__|    \___  >__/\_ \          \______  /\____/\____ |\___  >____  >
+                       \/      \/                 \/            \/    \/     \/  '''
 
-print("\n","\033[1;31m[Info]","\033[0;m","\033[0;33mVerification of passwords ([TREX-CODES])\n")
-print("\033[0;35m"," || wrotten in Python","\033[0;m")
-print("\033[0;35m"," || Coded by Trex-Codes")
-print("\033[0;35m"," || >> https://github.com/Trex-Codes\n","\033[0;m")
+print(Banner)                       
+print("\n",ERROR,"[Info]",MESSAGE,"Verification of passwords ([2 PERSONS OR MORE..])\n")
+print(MESSAGE2," || Wrotten in Python")
+print(MESSAGE2," || coded by Trex-Codes")
+print(MESSAGE2," || >> https://github.com/Trex-Codesn", END)
 
 def Person1():
 	contador = 0
 	while contador < 1:
 
 		Data = {
-			# Program, URL, User, Password
-			# HERE WRITE THE PASSWORD LIKE A DICCIONARY 
-			"Data01": "google ," "www.google.com ," "user123 ," "password123"
+			# Program-App, Url, user, password
+			"Gmail": "https://mail.google.com/mail/u/0/ ," "tfsf123@gmail.com ," "Hola123",
 		}
 
-		print("\n")
+		print('\n')
 		for key in Data:
-			print(key, end=" // ")
+			print(key, end=" || ")
 
-		print("\n")
-		print("\033[1;34m")
-		LogOption = input("  What you want to know: ")
-		print("\033[0;m")
+		print('\n')
+		print(MESSAGE4)
+		logOption = input("  What you want to know: ")
+		print(END)
 
-		if LogOption == "Data01":
-			print(Data['Data01'])
-		elif LogOption == "q" or LogOption == "q!" or LogOption == "quit":
-			print("\n")
-			print("\033[1;32m", "[+] SUCCESSFUL EXIT!!","\033[0;m")
+		if logOption == "Gmail" or logOption == "gmail" or logOption == "correo":
+			print(Data['Gmail'])
+		elif logOption == "q" or logOption == "!q" or logOption == "quit":
+			print('\n')
+			print(SUCCESS,"[+] SUCCESSFUL EXIT!!",END)
 			call('clear')
 			contador = 2
-		else: 
-			print(" \033[4;31m","Data no exist yet","\033[4;m")
+		else:
+			print(ERROR_DATA,"Data no exist yet",END)
 
 def Person2():
 	contador = 0
 	while contador < 1:
 
 		Data = {
-			# Program, URL, User, Password
-			# HERE WRITE THE PASSWORD LIKE A DICCIONARY 
-			"Data01": "google ," "www.google.com ," "user123 ," "password123"
+			# Program, Url, user, password
+			"Gmail": "https://mail.google.com/mail/u/0/ ," "tfsf123@gmail.com ," "Hola123",
+
 		}
 
-		print("\n")
+		print('\n')
 		for key in Data:
-			print(key, end=" // ")
+			print(key, end=" || ")
 
-		print("\n")
+		print('\n')
+		print(MESSAGE4)
+		logOption = input("  What you want to know: ")
+		print(END)
 
-		print("\033[1;34m")
-		LogOption = input("  What you want to know: ")
-		print("\033[0;m")
-
-		if LogOption == "Data01":
-			print(Data['Data01'])
-		elif LogOption == "q" or LogOption == "q!" or LogOption == "quit":
-			print("\033[1;32m","  [+] SUCCESSFUL EXIT!!","\033[0;m")
+		if logOption == "Gmail" or LogOption == "gmail" or LogOption == "correo":
+			print(Data['Gmail'])
+		elif logOption == "q" or LogOption == "!q" or LogOption == "quit":
+			print('\n')
+			print(SUCCESS,"[+] SUCCESSFUL EXIT!!",END)
 			call('clear')
 			contador = 2
-		else: 
-			print(" \033[4;31m","Data no exist yet","\033[4;m")
+		else:
+			print(ERROR_DATA,"Data no exist yet",END)
 
 cont = 1
 while cont != 0:
-
-	print("\033[1;34m")
-	optionPrinc = input("  ---------------------------------------------\n  [1] Check Password (Person1 - Person2)\n  [2] Generate filename.txt with all password\n  [q] Exit || Finish Program\n ----------------------------------------------  \n\n  [+] >> ")
-	print("\n")
-	print("\033[0;m")
+	print(MESSAGE4)
+	optionPrinc = input(" ---------------------------------------------\n  [1] Check Password (Person1 - Person2)\n  [q] Exit || Finish Program\n ---------------------------------------------\n\n [+] >> ")
+	print('\n')
+	print(END)
 
 	if optionPrinc == "1":
 		coun2 = 0
 		while coun2 == 0:
-
 			call('clear')
-			print("\033[1;35m")
-			a = input('  ---------------------------------------------\n  [?] Who you are (P1-P2)\n  [q] Back\n  --------------------------------------------- \n\n  [+] >> ')
-			call('clear')
-			print("\033[0;m")
+			print(MESSAGE3)
+			SelectPerson = input(' ---------------------------------------------\n  [?] Who you are (P1-P2)\n  [q] Back\n ---------------------------------------------\n\n [+] >> ')
+			call('clear')			
+			print(END)
 
-			if a == "p2": 
+			if SelectPerson == "p1":
 				call('clear')
-				Person2()
-				coun2 = 1
-			elif a == "p1":
 				Person1()
 				coun2 = 1
-			elif a == "q":
-				print("\n")
+			elif SelectPerson == "q":
+				print('\n')
 				coun2 = 1
-
-			else: 
-				print("\n")
-				print("\033[1;31m","  [!] Only (P1 or P2)","\033[0;m")
-
-	elif optionPrinc == "2":
-		print("# admin()")
+			else:
+				print('\n')
+				print(ERROR,"[!] Only (P1 or P2)", END)
 	elif optionPrinc == "q":
-		cont=cont-1
-		print("\033[1;32m"," [+] SUCCESSFUL EXIT!!","\033[0;m\n\n")
-		print("\033[1;32m","  ▄████▄  ▒█████  ███▄ ▄███▓█████     ▄▄▄▄   ▄▄▄      ▄████▄  ██ ▄█▀     ██████ ▒█████  ▒█████  ███▄    █     ▐██▌ ","\033[0;m")
-		print("\033[1;32m"," ▒██▀ ▀█ ▒██▒  ██▓██▒▀█▀ ██▓█   ▀    ▓█████▄▒████▄   ▒██▀ ▀█  ██▄█▒    ▒██    ▒▒██▒  ██▒██▒  ██▒██ ▀█   █     ▐██▌ ","\033[0;m")
-		print("\033[1;32m"," ▒▓█    ▄▒██░  ██▓██    ▓██▒███      ▒██▒ ▄█▒██  ▀█▄ ▒▓█    ▄▓███▄░    ░ ▓██▄  ▒██░  ██▒██░  ██▓██  ▀█ ██▒    ▐██▌ ","\033[0;m")
-		print("\033[1;32m"," ▒▓▓▄ ▄██▒██   ██▒██    ▒██▒▓█  ▄    ▒██░█▀ ░██▄▄▄▄██▒▓▓▄ ▄██▓██ █▄      ▒   ██▒██   ██▒██   ██▓██▒  ▐▌██▒    ▓██▒ ","\033[0;m")
-		print("\033[1;32m"," ▒ ▓███▀ ░ ████▓▒▒██▒   ░██░▒████▒   ░▓█  ▀█▓▓█   ▓██▒ ▓███▀ ▒██▒ █▄   ▒██████▒░ ████▓▒░ ████▓▒▒██░   ▓██░    ▒▄▄  ","\033[0;m")
-		print("\033[1;32m"," ░ ░▒ ▒  ░ ▒░▒░▒░░ ▒░   ░  ░░ ▒░ ░   ░▒▓███▀▒▒▒   ▓▒█░ ░▒ ▒  ▒ ▒▒ ▓▒   ▒ ▒▓▒ ▒ ░ ▒░▒░▒░░ ▒░▒░▒░░ ▒░   ▒ ▒     ░▀▀▒ ","\033[0;m")
-		print("\033[1;32m","   ░  ▒    ░ ▒ ▒░░  ░      ░░ ░  ░   ▒░▒   ░  ▒   ▒▒ ░ ░  ▒  ░ ░▒ ▒░   ░ ░▒  ░ ░ ░ ▒ ▒░  ░ ▒ ▒░░ ░░   ░ ▒░    ░  ░ ","\033[0;m")
-		print("\033[1;32m"," ░       ░ ░ ░ ▒ ░      ░     ░       ░    ░  ░   ▒  ░       ░ ░░ ░    ░  ░  ░ ░ ░ ░ ▒ ░ ░ ░ ▒    ░   ░ ░        ░ ","\033[0;m")
-		print("\033[1;32m"," ░ ░         ░ ░        ░     ░  ░    ░           ░  ░ ░     ░  ░            ░     ░ ░     ░ ░          ░     ░    ","\033[0;m")
-		print("\033[1;32m"," ░                                         ░         ░                                                             ","\033[0;m")
-		print("\033[1;32m"," 																												  ","\033[0;m")
+		cont = cont -1
+		print(SUCCESS,"[+] SUCCESSFUL EXIT!!",END,"\n")
+		Banner_End = SUCCESS + '''
+   ▄████▄   ▒█████   ███▄ ▄███▓▓█████     ▄▄▄▄    ▄▄▄       ▄████▄   ██ ▄█▀     ██████  ▒█████   ▒█████   ███▄    █     ▐██▌ 
+  ▒██▀ ▀█  ▒██▒  ██▒▓██▒▀█▀ ██▒▓█   ▀    ▓█████▄ ▒████▄    ▒██▀ ▀█   ██▄█▒    ▒██    ▒ ▒██▒  ██▒▒██▒  ██▒ ██ ▀█   █     ▐██▌ 
+  ▒▓█    ▄ ▒██░  ██▒▓██    ▓██░▒███      ▒██▒ ▄██▒██  ▀█▄  ▒▓█    ▄ ▓███▄░    ░ ▓██▄   ▒██░  ██▒▒██░  ██▒▓██  ▀█ ██▒    ▐██▌ 
+  ▒▓▓▄ ▄██▒▒██   ██░▒██    ▒██ ▒▓█  ▄    ▒██░█▀  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄      ▒   ██▒▒██   ██░▒██   ██░▓██▒  ▐▌██▒    ▓██▒ 
+  ▒ ▓███▀ ░░ ████▓▒░▒██▒   ░██▒░▒████▒   ░▓█  ▀█▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄   ▒██████▒▒░ ████▓▒░░ ████▓▒░▒██░   ▓██░    ▒▄▄  
+  ░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░   ░  ░░░ ▒░ ░   ░▒▓███▀▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒   ▒ ▒▓▒ ▒ ░░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒     ░▀▀▒ 
+    ░  ▒     ░ ▒ ▒░ ░  ░      ░ ░ ░  ░   ▒░▒   ░   ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░   ░ ░▒  ░ ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░░   ░ ▒░    ░  ░ 
+  ░        ░ ░ ░ ▒  ░      ░      ░       ░    ░   ░   ▒   ░        ░ ░░ ░    ░  ░  ░  ░ ░ ░ ▒  ░ ░ ░ ▒     ░   ░ ░        ░ 
+  ░ ░          ░ ░         ░      ░  ░    ░            ░  ░░ ░      ░  ░            ░      ░ ░      ░ ░           ░     ░    
+  ░                                            ░           ░                                                                 
+  		'''
+		print(Banner_End)
 		call('clear')
 	else:
 		call('clear')
-		print("\033[1;31m","[!] Option Wrong!!  ||  Only ([1], [2] [q])\n","\033[0;m")
+		print(ERROR,"[!] Option Wrong!! || Only ([1], [2], [q]\n",END)
+
